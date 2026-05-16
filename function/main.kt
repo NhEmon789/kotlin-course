e55c7bed1fbfcb274d629ed02666d9ee23f3580a
Md. Nakib Hasan Emon <160424627+NhEmon789@users.noreply.github.com>
2026-05-17 02:35:13 +0600

    Update main.kt

--- function/main.kt
+++ function/main.kt
@@ -4,6 +4,9 @@ fun main(){
     println("a= $a")
     sum(10,20)
     println(add(a))
+    println(square(2))
+    println(y(10,3))
+    println(operate(10,30, op = {a,b->a+b}))
 }
 //parameter
 fun sum(a:Int, b:Int){
@@ -14,4 +17,11 @@ fun sum(a:Int, b:Int){
 
 fun add(a:Int): Int{
     return a+a
+}
+fun square(x:Int):Int=x*x
+//lamda function/anonymous function
+val y={a:Int,b:Int ->a+b}
+//higher order function
+fun operate(a:Int, b:Int ,op:(Int,Int)-> Int):Int{
+    return op(a,b)
 }
